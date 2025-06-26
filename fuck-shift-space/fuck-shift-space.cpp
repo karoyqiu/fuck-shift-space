32921c11737b73a6e67fff760b7e4dc9340adeae
Roy QIU <karoyqiu@gmail.com>
2025-06-26 11:05:23 +0800

    注册应用重启。

--- fuck-shift-space/fuck-shift-space.cpp
+++ fuck-shift-space/fuck-shift-space.cpp
@@ -29,6 +29,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: 在此处放置代码。
+    RegisterApplicationRestart(L"-r", 0);
 
     // 初始化全局字符串
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
